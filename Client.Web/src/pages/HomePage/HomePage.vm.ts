d6bb63cf2a0dd58a0b9f7d6b9fa93d682633aa89
mvozbranyuk <mvozbranyuk@data-platform.ru>
2023-01-13 01:15:53 +0300

    HomePageVM modified (store access added, sendMessage method added)

--- Client.Web/src/pages/HomePage/HomePage.vm.ts
+++ Client.Web/src/pages/HomePage/HomePage.vm.ts
@@ -1,11 +1,14 @@
-import {ChatHubService, TYPES} from "@/services";
-import {ConnectionState, Message} from "@/types/services/ChatHubService";
-import {inject} from "inversify";
-import {makeObservable, observable} from "mobx";
+import { Store } from "@/data/store";
+import { ChatHubService, TYPES } from "@/services";
+import { ConnectionState, Message } from "@/types/services/ChatHubService";
+import { inject } from "inversify";
+import { makeObservable, observable } from "mobx";
 
 export class HomePageVM {
+	private _currentUser: string;
+
 	@observable
-	public chatHubService: ChatHubService;
+	private _chatHubService: ChatHubService;
 
 	@observable
 	public messages: Message[] = [];
@@ -14,10 +17,22 @@ export class HomePageVM {
 	public connectionState: ConnectionState;
 
 	constructor(
+		@inject(TYPES.STORE) store: Store,
 		@inject(TYPES.CHAT_HUB_SERVICE) chatHubService: ChatHubService
 	) {
-		this.chatHubService = chatHubService;
-		this.connectionState = chatHubService.state;
 		makeObservable(this);
+		this._currentUser = store.user || "anonymus";
+		this._chatHubService = chatHubService;
+		this.connectionState = chatHubService.state;
+		this._chatHubService.addMessagesListener((message) => {
+			this.messages.push(message);
+		});
+	}
+
+	async sendMessage(message: string) {
+		return await this._chatHubService.sendMessage(
+			this._currentUser,
+			message
+		);
 	}
 }
