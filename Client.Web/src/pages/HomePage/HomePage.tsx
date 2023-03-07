1126c48ba6230bce33519a19edd32db5d1bc8fd1
mvozbranyuk <mvozbranyuk@data-platform.ru>
2023-03-08 02:28:19 +0300

    add username setter

--- Client.Web/src/pages/HomePage/HomePage.tsx
+++ Client.Web/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,10 @@
 import {MessageForm, MessageList} from "@/components";
+import {Store} from "@/data";
+import {Layout} from "@/lib";
+import {TYPES} from "@/services/TYPES";
 import {DIcontainer} from "@/services/container";
 import {observer} from "mobx-react-lite";
+import {useEffect} from "react";
 import {HomePageVM} from "./HomePage.vm";
 import "./home-page.css";
 
@@ -9,10 +13,29 @@ const ObservableMessageList = observer(MessageList);
 export const HomePage = observer(() => {
 	const vm = DIcontainer.get(HomePageVM);
 
+	useEffect(() => {
+		let username = localStorage.getItem("cb$username");
+		
+		if (!username) {
+			username = window.prompt("Enter your username", "anonymous");
+
+			if (!username) {
+				username = "anonymous";
+			}
+
+			localStorage.setItem("cb$username", username);
+		}
+		
+		const store = DIcontainer.get<Store>(TYPES.STORE);
+		store.user = username;
+	}, []);
+
 	return (
-		<div id="home-page">
-			<ObservableMessageList data={vm.messages} />
-			<MessageForm onSubmit={(m) => vm.sendMessage(m)} />
-		</div>
+		<Layout>
+			<div id="home-page">
+				<ObservableMessageList data={vm.messages} />
+				<MessageForm onSubmit={(m) => vm.sendMessage(m)} />
+			</div>
+		</Layout>
 	);
 });
